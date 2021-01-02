c62f9fd2bd7e42198b748dc014252ac4ae20819e
TCFKARADENIZ <faruk.karadeniz@turkcell.com.tr>
2021-01-02 16:37:31 +0300

    converted components to functions

--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,17 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from './MovieCard';
 
-class MovieCardsList extends Component {
-  render() {
-    /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
-
-    const { profiles, users, movies } = this.props;
+function MovieCardsList(props) {
+    const { profiles, users, movies } = props;
     const usersByMovie = {};
-
     profiles.forEach(profile => {
       const movieID = profile.favoriteMovieID;
 
@@ -21,7 +13,6 @@ class MovieCardsList extends Component {
         usersByMovie[movieID] = [profile.userID];
       }
     });
-
     const movieCards = Object.keys(movies).map(id => (
       <MovieCard
         key={id}
@@ -30,12 +21,7 @@ class MovieCardsList extends Component {
         movieInfo={movies[id]}
       />
     ));
-
-    /*
-    Return JSX
-    */
     return <ul>{movieCards}</ul>;
-  }
 }
 
 export default MovieCardsList;
